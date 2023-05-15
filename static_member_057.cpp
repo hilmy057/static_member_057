98e4b28290a334b45c799af0371cbc281ce96461
hilmy057 <naufalh702@gmail.com>
2023-05-15 20:57:32 +0700

    first commit

---
+++ static_member_057.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include	<string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) : nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+ 
+void mahasiswa::setID() {
+	id = ++nim;
+
+}
+
+void mahasiswa::printAll() {
+	cout << " ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mahasiswa mhs3("andi kurniawan");
+	mahasiswa mhs4("joko purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+}
